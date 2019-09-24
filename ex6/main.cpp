f67e2c552eea02e1e6fbcd6276be9322e641ea71
--local <--local>
2019-09-24 19:04:31 -0300

    Corrigido o erro.

--- ex6/main.cpp
+++ ex6/main.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-long double *newton(long double *x)
+float *newton(float *x)
 {
 
-  long double v[2];
+  float v[2];
 
   for (int i = 0; i < 2; i++)
   {
@@ -14,25 +14,26 @@ long double *newton(long double *x)
   // x[0] -= (-sqrt(3 * pow(v[1], 2) - 5) / v[0]) + (3 / (2 * v[0])) * sqrt((-pow(v[0], 2) + 5) / 2);
   // x[1] -= (sqrt(3 * pow(v[1], 2) - 5) / (2 * v[1])) - (1 / (2 * v[1])) * sqrt((-pow(v[0], 2) + 5) / 2);
 
-  x[0] -= (((-5 * pow(v[0], 2)) + 5) / (10 * v[0]));
-  x[1] -= (((-5 * pow(v[1], 2)) + 10) / (10 * v[1]));
+  x[0] -= (((5 * pow(v[0], 2)) - 5) / (10 * v[0]));
+  x[1] -= (((5 * pow(v[1], 2)) - 10) / (10 * v[1]));
 
   return x;
 }
 
-void printX(long double *x)
+void printX(float *x)
 {
   for (int i = 0; i < 2; i++)
-    cout << "x" << i + 1 << " = " << x[i] << endl;
+    printf("x%d = %.8f\n", i + 1, x[i]);
+  //cout << "x" << i + 1 << " = " << x[i] << endl;
 }
 
 int main()
 {
 
-  long double *x;
+  float *x;
   int n;
 
-  x = (long double *)malloc(sizeof(long double) * 2);
+  x = (float *)malloc(sizeof(float) * 2);
 
   for (int i = 0; i < 2; i++)
     cin >> x[i];
