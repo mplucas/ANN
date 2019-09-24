933418c5ec6ac1041cdc863be25d66e60f01b085
--local <--local>
2019-09-24 19:16:59 -0300

    Melhorado

--- ex6/main.cpp
+++ ex6/main.cpp
@@ -3,16 +3,12 @@ using namespace std;
 
 float *newton(float *x)
 {
-
   float v[2];
 
   for (int i = 0; i < 2; i++)
   {
     v[i] = x[i];
   }
-
-  // x[0] -= (-sqrt(3 * pow(v[1], 2) - 5) / v[0]) + (3 / (2 * v[0])) * sqrt((-pow(v[0], 2) + 5) / 2);
-  // x[1] -= (sqrt(3 * pow(v[1], 2) - 5) / (2 * v[1])) - (1 / (2 * v[1])) * sqrt((-pow(v[0], 2) + 5) / 2);
 
   x[0] -= (((5 * pow(v[0], 2)) - 5) / (10 * v[0]));
   x[1] -= (((5 * pow(v[1], 2)) - 10) / (10 * v[1]));
@@ -24,12 +20,10 @@ void printX(float *x)
 {
   for (int i = 0; i < 2; i++)
     printf("x%d = %.8f\n", i + 1, x[i]);
-  //cout << "x" << i + 1 << " = " << x[i] << endl;
 }
 
 int main()
 {
-
   float *x;
   int n;
 
