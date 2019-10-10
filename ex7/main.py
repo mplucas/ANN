4d87384f32a8a384345342462e25c03f68bd15a6
--local <--local>
2019-10-10 19:35:06 -0300

    Limpeza de código

--- ex7/main.py
+++ ex7/main.py
@@ -1,15 +1,5 @@
 import numpy as np
 
-#--
-
-A = np.array([[1,1],[-3,1]])
-B = np.array([[6],[2]])
-
-A_inversa = np.linalg.inv(A)
-x = np.linalg.solve(A,B)
-
-#--
-
 xs = []
 ys = []
 
@@ -26,20 +16,7 @@ for i in range (0,len(xs)):
   for j in range (2,len(xs)):
     xs[i].append(pow(xs[i][1],j))
 
-# for i in range (0,len(xs)):
-#   print(xs[i])
-
-# for i in range (0,len(ys)):
-#   print(ys[i])
-
 ans = np.linalg.solve(xs,ys)
 
 for i in range (0,len(ans)):
   print('a'+str(i)+':',ans[i])
-
-# xs_inversa = np.linalg.inv(xs)
-# ans = np.dot(xs_inversa,ys)
-# print()
-
-# for i in range (0,len(ans)):
-#   print(ans[i])
