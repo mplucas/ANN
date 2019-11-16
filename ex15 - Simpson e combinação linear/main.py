fce65f974333ecf57c6a0281c821b39e6a1aca10
Lucas Meneghelli Pereira <lucas-m.p@hotmail.com>
2019-11-16 13:52:33 -0300

    Terminado ex15

--- ex15 - Simpson e combinação linear/main.py
+++ ex15 - Simpson e combinação linear/main.py
@@ -1,16 +1,38 @@
 import numpy as np
+import math
 
-def integrateX(expX, h, imin, imax):
-    acum = pow(imin,expX) + pow(imax,expX)
-    for i in np.arange( imin+h, imax, h ):
-        acum = acum + 2 * pow(i,expX)
-    return acum * (h/2)
+def calcF(f, x):
+    ans = 1
+    
+    if f[0] == 's':
+        ans = math.sin(int(f[1])*x)
+    elif f[0] == 'c':
+        ans = math.cos(int(f[1])*x)
 
-def integrateExp(expX, h, imin, imax):
-    acum = np.exp(-1.473*pow(imin,2))*pow(imin,expX) + np.exp(-1.473*pow(imax,2))*pow(imax,expX)
-    for i in np.arange( imin+h, imax, h ):
-        acum = acum + 2 * np.exp(-1.473*pow(i,2))*pow(i,expX)
-    return acum * (h/2)
+    if len(f) > 2:
+        if f[2] == 's':
+            ans = ans * math.sin(int(f[3])*x)
+        elif f[2] == 'c':
+            ans = ans * math.cos(int(f[3])*x)
+    return ans
+
+def integrateX(f, h, imin, imax):
+    n = int((imax-imin) / (2*h))
+    acum = calcF(f,imin) + calcF(f,imax)
+    for k in range( 1, n ):
+        acum = acum + 2 * calcF(f,imin+2*k*h)
+    for k in range( 1, n+1 ):
+        acum = acum + 4 * calcF(f,imin+(2*k-1)*h)
+    return acum * (h/3)
+
+def integrateExp(f, h, imin, imax):
+    n = int((imax-imin) / (2*h))
+    acum = np.exp(-1.19*pow(imin,2))*calcF(f,imin) + np.exp(-1.19*pow(imax,2))*calcF(f,imax)
+    for k in range( 1, n ):
+        acum = acum + 2 * np.exp(-1.19*pow(imin+2*k*h,2))*calcF(f,imin+2*k*h)
+    for k in range( 1, n+1 ):
+        acum = acum + 4 * np.exp(-1.19*pow(imin+(2*k-1)*h,2))*calcF(f,imin+(2*k-1)*h)
+    return acum * (h/3)
 
 def main():
     h = 0.01
@@ -18,12 +40,20 @@ def main():
     imax = 2
     A = []
     B = []
+
+    # Gerar matriz de formulas
+    fs = ['nn','s1','c1','s2','c2','s3','c3','s4','c4','s5','c5']
+    matrizf = []
+    for i in range(0,11):
+        matrizf.append([])
+        for j in range(0,11):
+            matrizf[i].append(fs[i]+fs[j])
     
-    for i in range(0,10):
-        B.append( integrateExp( i, h, -2, 2 ) )
+    for i in range(0,11):
+        B.append( integrateExp( fs[i], h, -2, 2 ) )
         A.append([])
-        for j in range(0,10):
-            A[i].append( integrateX( i+j, h, -2, 2 ) )
+        for j in range(0,11):
+            A[i].append( integrateX( matrizf[i][j], h, -2, 2 ) )
     
     X = np.linalg.solve(A,B)
 
